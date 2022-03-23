c68718784f48b22f81fbe35c1cd3a9bed72edc62
grethoremas3333 <grethoremas@gmail.com>
2022-03-23 15:28:12 +0100

    Initialisation du Dépôt le 23/03/2022 à 15:28

---
+++ src/com/experimentation/test/FileMap.java
@@ -0,0 +1,11 @@
+package com.experimentation.test;
+
+public class FileMap {
+    public static void main(String[] args) throws Exception{
+
+        System.out.println("Version:v0.1.0"  +"\n"+ "Initialisation du Programme...");
+
+        System.out.println("...Fin du Programme !!!");
+
+    }
+}
